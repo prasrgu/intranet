ecfbe0a90408ad0ad114d9f768bee22f334a25b0
1613112 <p.tontalapur@rgu.ac.uk>
2017-02-28 15:50:19 +0000

    my first php program

---
+++ week5/index.php
@@ -0,0 +1,18 @@
+<! DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+<?php
+echo "Hello World";
+?>
+</p>
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: 1613112
+ * Date: 28/02/2017
+ * Time: 15:38
+ */
